dfbc879c61c692fe89863b513ab4eb6477dbb69e
mikhailvitsen@gmail.com <mikhailvitsen@gmail.com@fb6254f8-8179-0bb1-db61-51de435bd17c>
2014-04-17 23:12:08 +0000

    messages loading works fine (with the help of messages package)

--- trunk/go/messages/message.go
+++ trunk/go/messages/message.go
@@ -8,11 +8,20 @@ import (
 )
 
 const (
-	MessageTypeText = 1
+	MessageTypeBase  = 0
+	MessageTypeText  = 1
+	MessageTypeLogin = 2
 )
 
-type Message struct {
-	MessageType int
+type Message interface {
+	SetType(MessageType uint32)
+	GetType() uint32
+	GetBytes() []byte
+	FromBytes(reader io.Reader)
+}
+
+type BaseMessage struct {
+	MessageType uint32
 }
 
 func WriteString(writer io.Writer, str string) {
@@ -21,35 +30,99 @@ func WriteString(writer io.Writer, str string) {
 }
 func ReadString(reader io.Reader) string {
 	var length uint16
-	fmt.Println(reader)
+	//fmt.Println(reader)
 	binary.Read(reader, binary.BigEndian, &length)
-	fmt.Println(length)
+	//fmt.Println(length)
 	var buf []byte = make([]byte, int(length))
 	reader.Read(buf)
 	return string(buf)
 }
 
-func (msg *Message) GetBytes() []byte {
+func (msg *BaseMessage) GetBytes() []byte {
 	return make([]byte, 0)
+}
+func (msg *BaseMessage) SetType(MessageType uint32) {
+	msg.MessageType = MessageType
+}
+func (msg *BaseMessage) GetType() uint32 {
+	return msg.MessageType
 }
 
 type TextMessage struct {
-	Message
+	BaseMessage
 	Text string
 }
 
 func (msg *TextMessage) GetBytes() []byte {
 	var buffer *bytes.Buffer = new(bytes.Buffer)
 
+	binary.Write(buffer, binary.BigEndian, uint32(msg.MessageType))
+
 	WriteString(buffer, msg.Text)
 	//var str string = ReadString(bytes.NewReader(buffer.Bytes()))
-	var str string = ReadString(buffer)
-
-	fmt.Println(str)
 
 	return buffer.Bytes()
+}
+func (msg *TextMessage) FromBytes(reader io.Reader) {
+	msg.Text = ReadString(reader)
+}
+
+func NewMessage(messageType uint32) Message {
+	var message Message
+	switch messageType {
+	case MessageTypeText:
+		message = new(TextMessage)
+	case MessageTypeLogin:
+		message = &LoginMessage{}
+	default:
+		panic("Unknown message type")
+	}
+
+	message.SetType(messageType)
+
+	return message
 }
 
 func OnlyForTest() {
 	fmt.Println("Hello!")
+}
+
+type LoginMessage struct {
+	BaseMessage
+	UserId   uint32
+	UserName string
+}
+
+func (msg *LoginMessage) GetBytes() []byte {
+	var buffer *bytes.Buffer
+
+	binary.Write(buffer, binary.BigEndian, uint32(msg.UserId))
+	WriteString(buffer, msg.UserName)
+
+	return buffer.Bytes()
+}
+func (msg *LoginMessage) FromBytes(reader io.Reader) {
+	binary.Read(reader, binary.BigEndian, msg.UserId)
+	msg.UserName = ReadString(reader)
+}
+
+func FromBytes(buffer []byte) Message {
+	reader := bytes.NewReader(buffer)
+	var messageType uint32
+	binary.Read(reader, binary.BigEndian, &messageType)
+	fmt.Println("Message type: ", messageType)
+
+	message := NewMessage(messageType)
+	switch message.FromBytes(reader); message.(type) {
+	case *TextMessage:
+		fmt.Println("it is text message!")
+	case *LoginMessage:
+		fmt.Println("it is login message!")
+	default:
+		fmt.Println("it is unknown message!")
+	}
+
+	fmt.Println(message)
+
+	return message
 }
